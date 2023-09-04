03b83b15521a7448b7edf54b48a108d227e76c2b
Artur Strzelecki <artur.strzelecki@gamedesire.com>
2023-09-04 20:13:18 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 setup(
     name='armada',
     packages=['armada'],
-    version='1.5',
+    version='1.6',
     description='Configuration manager for ArmadaPlatform services.',
     author='Ganymede',
     author_email='cerebro@ganymede.eu',
